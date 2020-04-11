7e4017c46747b29805a11b1932d2e31ffdf991be
Jilljoy <j-a-hill@hotmail.co.uk>
2020-04-11 22:00:31 +0100

    Added first person character controller

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,37 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// A quick and dirty camera controller
+/// </summary>
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(GetMovementInput());
-    }
 
-    private Vector3 GetMovementInput() {
-        Vector3 playerMovement = Vector3.zero;
-
-        if (Input.GetKey(KeyCode.W)) {
-            playerMovement.z += 1.0f;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            playerMovement.x -= 1.0f;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            playerMovement.z -= 1.0f;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            playerMovement.x += 1.0f;
-        }
-
-        return playerMovement * 0.1f;
-    }
 }
